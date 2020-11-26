7cce05135f6d2f66fbd64eafa480b7ef9237dcec
skyien-z <70281930+skyien-z@users.noreply.github.com>
2020-11-26 11:19:47 -0600

    Add files via upload

--- kz_scratchwork/slider_with_aggregate_districts.py
+++ kz_scratchwork/slider_with_aggregate_districts.py
@@ -1,40 +1,26 @@
 import pandas as pd
 import geopandas
 import streamlit as st
-import matplotlib.pyplot as plt
-from create_gdf import *
 import pydeck as pdk
-# import helpers
 
-dem_assignment_fname = 'data/wi_gerrymander_dem.csv'
-gop_assignment_fname = 'data/wi_gerrymander_rep.csv'
-population_fname = 'data/wi_tract_populations_census_2010.csv'
-voteshares_fname = 'data/wi_voteshares.csv'
+district_slider = st.slider('Select a district plan', 1, 83, 1)
+current_gdf = geopandas.read_file('geojson/wi_map_plan_' + str(district_slider) + '.geojson')
 
+INITIAL_VIEW_STATE = pdk.ViewState(latitude=44.155, longitude=-89.483492, zoom=6.3, max_zoom=16, pitch=45, bearing=0)
 
-# dem_plan = helpers.build_district_plan(tracts_fname, dem_assignment_fname, population_fname, voteshares_fname)
-# gdf = dem_plan.graph.data
+geojson = pdk.Layer(
+    "GeoJsonLayer",
+     data=current_gdf,
+     pickable=True,
+     auto_highlight=True,
+     get_fill_color='[15 + district*30, 150, 120]',
+     get_line_color='[255, 255, 255]',
+)
 
+r = pdk.Deck(layers=[geojson],
+            initial_view_state=INITIAL_VIEW_STATE,
+            mapbox_key='pk.eyJ1Ijoic2t5aWVuLXoiLCJhIjoiY2tnODJiaXRyMDl1OTJzbWtveTRsaGMwOSJ9.zFW9CBqmz3PAJ74FLRZRBA',
+            tooltip={'text': 'District: {district}\nPopulation: {population}\nDem Votes: {dem_votes}\nGOP Votes: {gop_votes}'}
+            )
 
-district_slider = st.slider('Select a district plan', 0, 83, 1)
-
-#st.write(map_as_gdf, district_slider)
-
-temp_gdf = geopandas.read_file('geojson/wi_map_plan_' + str(district_slider) + '.geojson')
-temp_gdf.plot(column = 'district')
-plt.axis('off')
-st.pyplot(plt)
-
-#st.map(map_as_gdf)
-
-# st.pydeck_chart(pdk.Deck(
-#      map_style='mapbox://styles/mapbox/light-v9',
-#      layers=[
-#         pdk.Layer(
-#             "GeoJsonLayer",
-#             data=map_as_gdf.district_plan_0,
-#             get_position=['lon', 'lat'],
-#
-#         ),
-#     ],
-# ))
+st.pydeck_chart(r)
