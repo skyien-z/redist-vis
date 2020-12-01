a0b3dfa3b886d82c0f5ba4ac4bd12951b61f5b8a
skyien-z <70281930+skyien-z@users.noreply.github.com>
2020-11-30 19:32:40 -0600

    Add files via upload

--- kz_scratchwork/slider_with_aggregate_districts.py
+++ kz_scratchwork/slider_with_aggregate_districts.py
@@ -2,8 +2,11 @@ import pandas as pd
 import geopandas
 import streamlit as st
 import pydeck as pdk
+import altair as alt
 
 st.title('Possible Wisconsin Districting Plans')
+
+# This section encodes the slider and map
 
 district_slider = st.slider('Select a district plan', 1, 83, 1)
 current_gdf = geopandas.read_file('geojson/wi_map_plan_' + str(district_slider) + '.geojson')
@@ -27,12 +30,38 @@ r = pdk.Deck(layers=[geojson],
 
 st.pydeck_chart(r)
 
-# Displays metrics on app sidebar
+#Displays metrics on app sidebar
+
 st.sidebar.subheader('Metrics for Plan ' + str(district_slider) + ":")
-st.sidebar.text('SL Index: ' + str(current_gdf.loc[1]['SL_index']))
-st.sidebar.text('Efficiency Gap: ' + str(current_gdf.loc[1]['efficiency_gap']))
-st.sidebar.text('Mean-Median Gap: ' + str(current_gdf.loc[1]['mm_gap']))
+
+metric_descriptions = [ 'SL Index: ' + str(current_gdf.loc[1]['SL_index']),
+                        'Efficiency Gap: ' + str(current_gdf.loc[1]['efficiency_gap']),
+                        'Mean-Median Gap: ' + str(current_gdf.loc[1]['mm_gap'])]
+
 st.sidebar.text('')
+
+# This section encodes the metric graphs
+metric_gdf_column_names = ['SL_index', 'efficiency_gap', 'mm_gap']
+metrics_df = pd.read_json('geojson/all_plan_metrics')
+current_metric_df = pd.DataFrame(metrics_df.loc[district_slider]).transpose()
+
+for i in range(0, 3):
+    st.sidebar.text(metric_descriptions[i])
+    altair_metric_chart = alt.Chart(metrics_df).mark_point(filled=True, size=50).encode(
+        x='plan',
+        y=metric_gdf_column_names[i],
+    ).properties(
+        width=250,
+        height=250
+    )
+
+    highlighted_plan_points = alt.Chart(current_metric_df).mark_point(filled=True, size=100).encode(
+        x='plan',
+        y=metric_gdf_column_names[i],
+    color=alt.value('yellow')
+    )
+
+    st.sidebar.write(altair_metric_chart + highlighted_plan_points)
 
 st.sidebar.text('Party Votes per District:')
 votes_data_df = current_gdf.drop(columns=['district', 'population', 'dem_voteshare', 'gop_voteshare', 'geometry', 'SL_index', 'efficiency_gap', 'mm_gap'])
